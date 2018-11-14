ff53c514b4ce98b6abb82bd2d16fe32064465f1c
Yohovani Vargas <yohov@DESKTOP-Q0S5241>
2018-11-14 10:29:11 -0600

    Se modifico recomendación de usuario

--- recomendarUsuario.php
+++ recomendarUsuario.php
@@ -3,19 +3,20 @@
 include 'conexion.php';
 if(!preg_match("[^A-Za-z0-9]",$_POST['nombre'])){
 	$nombre = $_POST['nombre'];
+	$nombre = strtolower($nombre);
 	do{
 		$SQL =  "SELECT usuario FROM usuarios WHERE usuario = '".$nombre."'";
 		$resultado = mysqli_query($conexion,$SQL) or die(mysqli_error($conexion));
 		if(mysqli_num_rows($resultado) != 0){
-			$nombre += randomText(1)."";
+			$nombre .= randomText(1)."";
 		}
 	}while(mysqli_num_rows($resultado) != 0);
 	echo '<div class="alert alert-success">
 			<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-			<strong>Usuario Recomendado:</strong> <label id="nameUsuario">'.$nombre.'</label>
+			<strong>Nombre de usuario:</strong> <label id="nameUsuario">'.$nombre.'</label>
 		</div>
 		<div class="col-sm-10">
-			<input type="text" class="form-control" required name="user" placeholder="Nombre de usuario" id="user" value="'.$nombre.'">
+			<input type="text" readonly class="form-control" required name="user" placeholder="Nombre de usuario" id="user" value="'.$nombre.'">
 		</div>';
 	
 
