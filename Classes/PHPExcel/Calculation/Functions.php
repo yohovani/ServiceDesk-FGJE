68f5d9d52ee6d84d5caec71f0ab3eee55b806533
Yohovani Vargas <yohov@DESKTOP-Q0S5241>
2019-06-13 13:19:26 -0500

    Se corrigio error en la libreria de excel x3

--- Classes/PHPExcel/Calculation/Functions.php
+++ Classes/PHPExcel/Calculation/Functions.php
@@ -555,16 +555,16 @@ class PHPExcel_Calculation_Functions
             case 'float':
             case 'integer':
                 return $value;
-				break;
+				//break;
             case 'boolean':
                 return (integer) $value;
-				break;
+				//break;
             case 'string':
                 //    Errors
                 if ((strlen($value) > 0) && ($value{0} == '#')) {
                     return $value;
                 }
-                break;
+                //break;
         }
         return 0;
     }
